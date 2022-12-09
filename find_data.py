3c3688ede174e5888e7dc3cdce47999accc5e948
CrazyDevNEW <tel123404@gmail.com>
2022-12-09 13:11:22 +0500

    MainCommit

---
+++ find_data.py
@@ -0,0 +1,20 @@
+class FindData():
+    def find_date(self):
+        # получаем дату
+        string_date = self.calendarWidget.selectedDate().getDate()
+        # добавляем ноль, елси месяц <= 9
+        if int(string_date[1]) <= 9:
+            string_date = (string_date[0], '0' + str(string_date[1]), string_date[-1])
+        # добавляем ноль, елси день <= 9
+        if int(string_date[2]) <= 9:
+            string_date = (string_date[0], str(string_date[1]), '0' + str(string_date[-1]))
+        # берем текст из line edit
+        line_edit = self.lineEdit.text()
+        # задаем словарю новое значение или переопределяем старое
+        self.all_dates[
+            f'{string_date[0]}-{string_date[1]}-{string_date[2]}-{self.timeEdit.time().toString()}'] = line_edit
+        # изюавляемся от повторов
+        self.textBrowser.clear()
+        # сортируем даты и выводим их
+        for key in sorted(self.all_dates.keys()):
+            self.textBrowser.append(f'{key} - {self.all_dates[key]}')
